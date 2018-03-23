a629588dd45a7ecfd8e7713ddaa3584f2d3c9078
Viktoria <vikitikitavi@users.noreply.github.com>
2018-03-23 16:01:58 +0200

    Added login_required usage example (#12)
    * [examples] add login_requiered decorator usage example
    
    * [example] add login_requiered decorator usage example
    
    * [example] login_required rename function for clarity
    
    * [example] fix typo filename loggin_required -> login_required

---
+++ example/login_required.py
@@ -0,0 +1,37 @@
+import jwt
+from aiohttp import web
+
+from aiohttp_jwt import JWTMiddleware, login_required
+
+sharable_secret = 'secret'
+
+
+async def public_handler(request):
+    return web.json_response({
+        'username': request['user'].get('username')
+        if 'user' in request else 'anonymous',
+    })
+
+
+@login_required
+async def auth_required_handler(request):
+    return web.json_response({
+        'username': request['user'],
+    })
+
+app = web.Application(
+    middlewares=[
+        JWTMiddleware(
+            secret_or_pub_key=sharable_secret,
+            request_property='user',
+            credentials_required=False,
+        )
+    ]
+)
+
+
+app.router.add_get('/public', public_handler)
+app.router.add_get('/protected', auth_required_handler)
+
+if __name__ == '__main__':
+    web.run_app(app)
